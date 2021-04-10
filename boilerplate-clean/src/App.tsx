27e5f704d8650bdbe9274f6c79c23b5a99759c2f
Allan Thomas <allanthomasof@gmail.com>
2021-04-09 22:39:34 -0300

    first commit

---
+++ boilerplate-clean/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GlobalStyle } from "./syles/global";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <GlobalStyle/>
+    </div>
+  );
+}
