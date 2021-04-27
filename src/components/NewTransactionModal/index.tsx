634826ebaac5286b27fa9ca965cfdd0f5710cbbb
Allan Thomas <allanthomasof@gmail.com>
2021-04-26 21:24:39 -0300

    feat: add form newTransactionModel and styles

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,75 @@
+import Modal from "react-modal";
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
+import { useState } from "react";
+
+Modal.setAppElement('#root')
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit')
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal"/>
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+
+        <input
+          placeholder="Valor"
+          type="number"
+        />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            isActive={type === 'deposit'}
+            onClick={() => { setType('deposit') }}
+          >
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            isActive={type === 'withdraw'}
+            onClick={() => { setType('withdraw') }}
+          >
+            <img src={outcomeImg} alt="Saída"/>
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+    </Modal>
+  );
+}
